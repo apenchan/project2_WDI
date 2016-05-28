8ac48488d61fe4b7f762308016157efa88a3002d
apenchan <a.penchansky@gmail.com>
2016-05-28 13:18:03 -0400

    still a test

--- server.js
+++ server.js
@@ -6,3 +6,15 @@ var db = process.env.MONGODB_URI || "mongodb://localhost/Chuck"//this is the dat
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
 var port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(methodOverride(function(req, res){
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
