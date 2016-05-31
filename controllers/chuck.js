7a3b96c20a8818cf24a2a2008035b6830cef4db5
apenchan <a.penchansky@gmail.com>
2016-05-31 17:55:58 -0400

    fixing external API for deployment

--- controllers/chuck.js
+++ controllers/chuck.js
@@ -49,7 +49,8 @@ router.get('/random_joke', function(req, res) {
 	    	joke: x.value.joke
 	    }
 	    // console.log(random_joke);
-	    res.render('home.ejs', {random_joke});
+	    console.log(random_joke);
+	    // res.render('home.ejs', {random_joke});
 	    // var response = JSON.parse(body);
 
 	  }
